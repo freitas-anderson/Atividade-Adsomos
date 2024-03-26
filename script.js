a5ea7c2227bc22615526cf2ac41545029983d1b9
Anderson Freitas <andersonfreitasa949@gmail.com>
2024-03-26 14:45:24 -0300

    commmit

---
+++ script.js
@@ -0,0 +1,16 @@
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    if (menuMobile.classList.contains("open")){
+        menuMobile.classList.remove("open");
+        document.querySelector(".icon").src = "imagens/menu_white_36dp.svg";
+    } else{
+        menuMobile.classList.add("open");
+        document.querySelector(".icon").src = "imagens/close_white_36dp.svg";
+    }
+}
+
+function submitForm() {
+    var searchTerm = document.getElementById("searchInput").value;
+
+    console.log("Termo de pesquisa enviado: " + searchTerm);
+  }
